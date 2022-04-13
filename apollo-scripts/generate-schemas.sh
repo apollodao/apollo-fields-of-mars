10375c87e29c4121922f681d43580d22f6b995d9
othermike <othermike@apollo.farm>
2022-04-13 12:08:47 -0400

    feat: update schema script

--- apollo-scripts/generate-schemas.sh
+++ apollo-scripts/generate-schemas.sh
@@ -1,5 +1,4 @@
-json2ts -i ../contracts/martian-field/schema/ -o ./
-
+npx json-schema-to-typescript  -i ../contracts/martian-field/schema/ -o ./
 
 for f in ./*.d.ts; do
     mv -- "$f" "${f%.d.ts}.ts"
